2bd2b09c91b62732f7be1a3f69ce669a79655804
Besrat Zerehaimanot <bzerehaimanot@gmail.com>
2020-09-17 21:52:52 -0400

    5min mark video 4

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-     <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
+           <b-field label="Username"
+            type="is-success"
+            message="This username is available">
+            <b-input value="johnsilver" maxlength="30"></b-input>
         </b-field>
-       <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
+
+        <b-field label="Password"
+            type="is-warning"
+            :message="['Password is too short', 'Password must have at least 8 characters']">
+            <b-input value="123" type="password" maxlength="30"></b-input>
         </b-field>
-    
+
   </div>
 </template>
 
